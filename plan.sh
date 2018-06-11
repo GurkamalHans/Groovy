7aed926f8ae55c1d4eb768b20004f489402dc4b3
GurkamalHans <gurkamalhans@hotmail.com>
2018-06-11 12:00:04 -0400

    update

--- plan.sh
+++ plan.sh
@@ -3,7 +3,7 @@ pkg_name=groovy
 pkg_version=0.1.0
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="Installs groovy"
-pkg_deps=(core/jre8 core/openssh core/curl)
+pkg_deps=(core/jre8 core/openssh core/curl core/unzip)
 
 do_verify() {
 	return 0;
