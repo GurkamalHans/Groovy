fcfe7c6a29e92b847c9e31639d2c12eb10441c41
GurkamalHans <gurkamalhans@hotmail.com>
2018-06-11 11:23:48 -0400

    groovy

---
+++ plan.sh
@@ -0,0 +1,24 @@
+pkg_origin=gurkamalhans
+pkg_name=groovy
+pkg_version =0.1.0
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_description="Installs groovy"
+pkg_deps=(core/jre8 core/openssh)
+
+do_verify() {
+	return 0;
+}
+
+do_unpack() {
+	return 0;
+}
+
+do_build() {
+	return 0;
+}
+
+do_install() {
+	curl -s get.sdkman.io | bash
+	source "$HOME/.sdkman/bin/sdkman-init.sh"
+	sdk install groovy
+}
